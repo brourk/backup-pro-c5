7e4f82c0fb25444b88eec28ddff8e22991ced957
Eric Lamb <eric@mithra62.com>
2016-02-06 19:05:59 -0800

    Adds files and license setting view partials

--- packages/backup_pro/single_pages/dashboard/backup_pro/settings/_files.php
+++ packages/backup_pro/single_pages/dashboard/backup_pro/settings/_files.php
@@ -1,1 +1,61 @@
-<?php
+<fieldset>
+    <div class="row">
+    	<div class="form-group <?php if($form_errors['max_file_backups']): ?>has-error<?php endif; ?>">
+    		<label for="max_file_backups" class="control-label col-sm-3"><?php echo t($view_helper->m62Lang('max_file_backups'))?></label>
+    		<div class="col-sm-7">
+    			<div class="input-group">
+    			<?php echo $form->text('max_file_backups', $form_data['max_file_backups'])?>
+    			<span class="input-group-addon"><i class="fa fa-asterisk"></i></span>
+    			</div>
+    			<div class="help-block small"><?php echo $view_helper->m62Lang('max_file_backups_instructions'); ?></div>
+    			<?php echo $view_helper->m62FormErrors($form_errors['max_file_backups']); ?>
+    		</div>
+    	</div>
+    </div>
+    
+    <div class="row">
+    	<div class="form-group <?php if($form_errors['file_backup_alert_threshold']): ?>has-error<?php endif; ?>">
+    		<label for="file_backup_alert_threshold" class="control-label col-sm-3"><?php echo t($view_helper->m62Lang('file_backup_alert_threshold'))?></label>
+    		<div class="col-sm-7">
+    			<div class="input-group">
+    			<?php echo $form->text('file_backup_alert_threshold', $form_data['file_backup_alert_threshold'])?>
+    			<span class="input-group-addon"><i class="fa fa-asterisk"></i></span>
+    			</div>
+    			<div class="help-block small"><?php echo $view_helper->m62Lang('file_backup_alert_threshold_instructions'); ?></div>
+    			<?php echo $view_helper->m62FormErrors($form_errors['file_backup_alert_threshold']); ?>
+    		</div>
+    	</div>
+    </div>  
+    
+    <div class="row">
+    	<div class="form-group <?php if($form_errors['backup_file_locations']): ?>has-error<?php endif; ?>">
+    		<label for="backup_file_locations" class="control-label col-sm-3"><?php echo t($view_helper->m62Lang('backup_file_locations'))?></label>
+    		<div class="col-sm-7">
+    		<?php echo $form->textarea('backup_file_locations', $form_data['backup_file_locations']); ?>
+    		<div class="help-block small"><?php echo $view_helper->m62Lang('backup_file_locations_instructions'); ?></div>
+    		<?php echo $view_helper->m62FormErrors($form_errors['backup_file_locations']); ?>
+    		</div>
+    	</div>
+    </div>
+    
+    <div class="row">
+    	<div class="form-group <?php if($form_errors['exclude_paths']): ?>has-error<?php endif; ?>">
+    		<label for="exclude_paths" class="control-label col-sm-3"><?php echo t($view_helper->m62Lang('exclude_paths'))?></label>
+    		<div class="col-sm-7">
+    		<?php echo $form->textarea('exclude_paths', $form_data['exclude_paths']); ?>
+    		<div class="help-block small"><?php echo $view_helper->m62Lang('exclude_paths_instructions'); ?></div>
+    		<?php echo $view_helper->m62FormErrors($form_errors['exclude_paths']); ?>
+    		</div>
+    	</div>
+    </div>  
+    
+    <div class="row">
+    	<div class="form-group <?php if($form_errors['regex_file_exclude']): ?>has-error<?php endif; ?>">
+        	<label class="control-label col-sm-3" for="regex_file_exclude"><?php echo t($view_helper->m62Lang('regex_file_exclude'))?></label>
+        	<div class="col-sm-7">
+                <?php echo $form->checkbox('regex_file_exclude', 1, $form_data['regex_file_exclude'])?>
+                <span class="small"><?php echo $view_helper->m62Lang('regex_file_exclude_instructions'); ?></span>
+            </div>
+        </div>
+    </div>    
+</fieldset>
--- packages/backup_pro/single_pages/dashboard/backup_pro/settings/_license.php
+++ packages/backup_pro/single_pages/dashboard/backup_pro/settings/_license.php
@@ -1,1 +1,16 @@
-<?php
+<fieldset>
+<p><?php echo $view_helper->m62Lang('license_details_instructions'); ?></p>
+    <div class="row">
+    	<div class="form-group <?php if($form_errors['license_number']): ?>has-error<?php endif; ?>">
+    		<label for="license_number" class="control-label col-sm-3"><?php echo t($view_helper->m62Lang('license_number'))?></label>
+    		<div class="col-sm-7">
+    			<div class="input-group">
+    			<?php echo $form->text('license_number', $form_data['license_number'])?>
+    			<span class="input-group-addon"><i class="fa fa-asterisk"></i></span>
+    			</div>
+    			<div class="help-block small"><?php echo $view_helper->m62Lang('license_number_instructions'); ?></div>
+    			<?php echo $view_helper->m62FormErrors($form_errors['license_number']); ?>
+    		</div>
+    	</div>
+    </div>
+</fieldset>    
