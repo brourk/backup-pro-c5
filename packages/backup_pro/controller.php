3fe6b5ed46ffdd3bf4d33d6206faa57ffc796e81
Eric Lamb <eric@mithra62.com>
2016-02-10 17:26:40 -0800

    Updates install to remove errant front end nav item

--- packages/backup_pro/controller.php
+++ packages/backup_pro/controller.php
@@ -88,7 +88,16 @@ class Controller extends Package
         $sp = SinglePage::add('/dashboard/backup_pro/manage', $pkg);
         $sp->setAttribute('exclude_nav', true);
         
+        $sp = SinglePage::add('/backup_pro', $pkg);
+        $sp->setAttribute('exclude_page_list', true);
+        $sp->setAttribute('exclude_sitemapxml', true);
+        $sp->setAttribute('exclude_search_index', true);
+        $sp->setAttribute('exclude_nav', true);
+        
         $sp = SinglePage::add('/backup_pro/cron', $pkg);
+        $sp->setAttribute('exclude_page_list', true);
+        $sp->setAttribute('exclude_sitemapxml', true);
+        $sp->setAttribute('exclude_search_index', true);
         $sp->setAttribute('exclude_nav', true);
         return $pkg;
         
