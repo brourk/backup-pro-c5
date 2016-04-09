a3307850bf4044b021bccba36983cd2e755313da
Eric Lamb <eric@mithra62.com>
2016-04-08 17:01:03 -0700

    Updates version

--- packages/backup_pro/controller.php
+++ packages/backup_pro/controller.php
@@ -44,7 +44,7 @@ class Controller extends Package
      * Versoin of Backup Pro 
      * @var string
      */
-    protected $pkgVersion = '3.3.0.1';
+    protected $pkgVersion = '3.3.0.2';
 
     /**
      * (non-PHPdoc)
