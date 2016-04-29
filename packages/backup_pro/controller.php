d703297b02e3fbfebd37db9884951ef5382fc230
Eric Lamb <eric@mithra62.com>
2016-04-29 15:28:30 -0700

    updates build

--- packages/backup_pro/controller.php
+++ packages/backup_pro/controller.php
@@ -44,7 +44,7 @@ class Controller extends Package
      * Versoin of Backup Pro 
      * @var string
      */
-    protected $pkgVersion = '3.3.0.3';
+    protected $pkgVersion = '3.3.0.4';
 
     /**
      * (non-PHPdoc)
