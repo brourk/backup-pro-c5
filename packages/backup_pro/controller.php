4638d6cfd4c8d2bee5b97787660fd48eb99033e2
Eric Lamb <eric@mithra62.com>
2016-05-13 04:32:23 -0700

    bumps version

--- packages/backup_pro/controller.php
+++ packages/backup_pro/controller.php
@@ -44,7 +44,7 @@ class Controller extends Package
      * Versoin of Backup Pro 
      * @var string
      */
-    protected $pkgVersion = '3.3.0.5';
+    protected $pkgVersion = '3.3.0.6';
 
     /**
      * (non-PHPdoc)
