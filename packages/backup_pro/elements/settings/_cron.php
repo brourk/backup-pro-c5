56c5d1a8eb0bf6539feb0db887b69d0aa499b073
Eric Lamb <eric@mithra62.com>
2016-07-24 20:48:06 -0700

    adds ids for testing

--- packages/backup_pro/elements/settings/_cron.php
+++ packages/backup_pro/elements/settings/_cron.php
@@ -14,10 +14,10 @@
         <tr class="even">
         	<td width='50%' style="width:50%;"><?php echo $view_helper->m62Lang($key); ?></td>
         	<td style="width:50%;">
-        		<div class="select_all"><?php echo 'curl '.$context->url($cron['url']); ?></div>
+        		<div class="select_all" id="cron_cmd_<?php echo $key; ?>"><?php echo 'curl '.$context->url($cron['url']); ?></div>
         	</td>
         	<td style="width:50%;">
-        		<a href="<?php echo $context->url($cron['url']); ?>" class="test_cron" rel="<?php echo $key; ?>">
+        		<a href="<?php echo $context->url($cron['url']); ?>" class="test_cron" rel="<?php echo $key; ?>" id="cron_url_<?php echo $key; ?>">
         			<img src="<?php echo $bp_static_path; ?>/images/test.png" />
         		</a> <img src="<?php echo $bp_static_path; ?>/images/indicator.gif" id="animated_<?php echo $key; ?>" style="display:none" />
         	</td>
